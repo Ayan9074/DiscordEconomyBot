894e192f9c9d5d10c64d469b5fe36de26d2f9459
Ayan9074 <57524544+Ayan9074@users.noreply.github.com>
2020-12-27 15:46:52 +0400

    Uploading Discord Bot

---
+++ mainbot.py
@@ -0,0 +1,71 @@
+import discord
+from discord.ext import commands
+import json
+import os
+import random 
+
+os.chdir("C:\\Users\\Ayan\\Documents\\YoutubeVideos")
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+@client.command()
+async def bal(ctx):
+    await open_account(ctx.author)
+    user = ctx.author
+    users = await get_bank_data()
+
+    wallet_amt = users[str(user.id)]["wallet"]
+    bank_amt = users[str(user.id)]["bank"]
+
+    em = discord.Embed(title = f"{ctx.author.name}'s balance", color = discord.Color.red())
+    em.add_field(name = "wallet balance", value = wallet_amt)
+    em.add_field(name = "bank balance", value = bank_amt)
+    await ctx.send(embed = em)
+
+
+@client.command()
+async def beg(ctx):
+    await open_account(ctx.author)
+    
+    users = await get_bank_data()
+
+    user = ctx.author
+    
+    earnings = random.randrange(101)
+
+    await ctx.send(f"Someone gave you {earnings} coins!!")
+
+
+    users[str(user.id)]["wallet"] += earnings
+
+    with open("mainbank.json", "w") as f:
+        json.dump(users, f)
+
+
+async def open_account(user):
+
+    users = await get_bank_data()
+
+    if str(user.id) in users:
+        return False
+    else:
+        users[str(user.id)] = {}
+        users[str(user.id)]["wallet"] = 0
+        users[str(user.id)]["bank"] = 0
+    
+    
+    with open("mainbank.json", "w") as f:
+        json.dump(users, f)
+    return True
+
+async def get_bank_data():
+    with open("mainbank.json","r") as f:
+        users = json.load(f)
+    return users
+
+
+client.run('NzkyNjQ1OTIxMjc1NjQxODg3.X-gvCg.Dz_v_Iy7w1JvjUjyXCVAyCjGj_E')
